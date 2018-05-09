97b1eb7ade773b9a7e7538e52f829ed4151e680e
wxiaosui_xiongda <wangxsui@gmail.com>
2018-05-09 19:03:24 +0800

    123

--- demo-hot/main.js
+++ demo-hot/main.js
@@ -1,5 +1,9 @@
 (function () {
     var hot = document.getElementsByClassName('hot')[0];
-    var text = document.createTextNode("hello,world");
+    var text = document.createTextNode("hello,worlder````");
     hot.appendChild(text);
+
+    if (module.hot) {
+		console.log('11111')
+	}
 })()
